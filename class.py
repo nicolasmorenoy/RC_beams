e4a735fce6a507094e492fa990d2c450a5ea7054
Nicolás <nicolasmorenoy@gmail.com>
2021-08-25 14:58:10 -0500

    Actualización script de clases y functions

--- class.py
+++ class.py
@@ -1,10 +1,10 @@
 import math
 PI=math.pi
-WAs = 7850
+from functions import _rebar_properties, Mn
 
 
-class Beams:
-    def __init__(self, _name, _b, _h, _r,_NS,_DS,_NI, _DI, _NT, _DT, _ST):
+class Beam:
+    def __init__(self, _name, _b, _h, _r,_NS,_DS,_NI, _DI, _NT, _DT, _ST, fc, fy):
         self._name= _name
         self._b=_b
         self._h=_h
@@ -16,36 +16,43 @@ class Beams:
         self._NT=_NT
         self._DT=_DT
         self._ST=_ST
+        self.fc=fc
+        self.fy=fy
     
     def _reinforcement_properties(self):
-        ASproperties=Reinforcement_data(self._DS)
-        dAsS=ASproperties[0]
-        aAsS=ASproperties[1]
-        PAsS=ASproperties[2]
-        WrebarS=ASproperties[3]
-        AIproperties=Reinforcement_data(self._DI)
-        dAsI=AIproperties[0]
-        aAsI=AIproperties[1]
-        PAsI=AIproperties[2]
-        WrebarI=AIproperties[3]
-        ATproperties=Reinforcement_data(self._DT)
-        dAsT=ATproperties[0]
-        aAsT=ATproperties[1]
-        PAsT=ATproperties[2]
-        WrebarT=ATproperties[3]
+        #First, we calculate the properties for the diferents diameters of the beam's reinforcement
+        ASproperties=_rebar_properties(self._DS)
+        self.dAsS=ASproperties['diameter']
+        self.aAsS=ASproperties['area']
+        self.PAsS=ASproperties['perimeter']
+        self.WrebarS=ASproperties['weight']
+        AIproperties=_rebar_properties(self._DI)
+        self.dAsI=AIproperties['diameter']
+        self.aAsI=AIproperties['area']
+        self.PAsI=AIproperties['perimeter']
+        self.WrebarI=AIproperties['weight']
+        ATproperties=_rebar_properties(self._DT)
+        self.dAsT=ATproperties['diameter']
+        self.aAsT=ATproperties['area']
+        self.PAsT=ATproperties['perimeter']
+        self.WrebarT=ATproperties['weight']
 
-
-
-class Reinforcement_data:
-    def __init__(self, diameter):
-        self._diameter = diameter
+        #Then, acording to the input we calculate the beam's reinforcement
+        self.As_S=self._NS*self.aAsS
+        self.As_I=self._NI*self.aAsI
+        self.As_T=self._NT*self.aAsT
     
-    def _properties(self):
-        dAs= self.diameter/8*.0254
-        aAs = PI*dAs**2/4
-        PAs = PI*dAs
-        Wrebar=WAs*dAs
-        _rebar_properties =list[dAs, aAs, PAs, Wrebar]
-        
-        return _rebar_properties
+    def _beam_properties(self):
+        self.Ag=self._b*self._h
+        self.ds=self._h-self._r-self.dAsT-self.dAsS/2
+        self.di=self._h-self._r-self.dAsT-self.dAsI/2
+        self.Ig=self._b*self._h**3/12
+        self.ro_S=self._As_S/self._b/self.ds
+        self.ro_I=self._As_I/self._b/self.di
+    
+    def _nominal_properties(self):
+        self.Mn_S=Mn(self._b,self.ds,self.fc,self.fy,self.ro_S)
+
+
+
 
---
+++ functions.py
@@ -0,0 +1,18 @@
+import math
+PI=math.pi
+WAs = 7850
+
+
+def _rebar_properties(dA):
+    dAs= dA/8*.0254
+    aAs = PI*dAs**2/4
+    PAs = PI*dAs
+    Wrebar=WAs*dAs
+    rebar_properties ={'diameter': dAs,'area': aAs,'perimeter': PAs,'weight': Wrebar}
+    return _rebar_properties
+
+
+def Mn(b,d,fc,fy,ro):
+    Mn=ro*b*d*d*fy*(1-0.59*ro*fy/fc)*1000
+
+    return Mn
