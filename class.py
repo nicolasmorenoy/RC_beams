773ad736c84103e4a376271559fe9bedf1bbafbe
unknown <nicolasmorenoy@gmail.com>
2021-08-28 16:23:21 -0500

    Actualización clases con comentarios

--- class.py
+++ class.py
@@ -4,31 +4,36 @@ from functions import _rebar_properties, Mn
 
 
 class Beam:
-    def __init__(self, _name, _b, _h, _r,_NS,_DS,_NI, _DI, _NT, _DT, _ST, fc, fy):
-        self._name= _name
-        self._b=_b
-        self._h=_h
-        self._r=_r
-        self._NS=_NS
-        self._DS=_DS
-        self._NI=_NI
-        self._DI=_DI
-        self._NT=_NT
-        self._DT=_DT
-        self._ST=_ST
+    #We initialize the class with the basic information about the beam like 
+    # b=base, h=hight, r=recover 
+    # NS/I/T=Number of superior/inferior/transverse reinforcement bars
+    # ST Longitudinal separation of transverse reinforcement bars
+    # fc and fy the material properties
+    def __init__(self, name, b, h, r,NS,DS,NI, DI, NT, DT, ST, fc, fy):
+        self._name= name
+        self._b=b
+        self._h=h
+        self._r=r
+        self._NS=NS
+        self._DS=DS
+        self._NI=NI
+        self._DI=DI
+        self._NT=NT
+        self._DT=DT
+        self._ST=ST
         self.fc=fc
         self.fy=fy
-        ASS=Reinforcement(self._DS)
+        ASS=Reinforcement(self._DS, self._NS)
         self.dAsS= ASS.dAs
         self.aAsS=ASS.aAs 
         self.PAsS=ASS.PAs
         self.WrebarS=ASS.Wrebar
-        ASI=Reinforcement(self._DI)
+        ASI=Reinforcement(self._DI, self._NI)
         self.dAsI= ASI.dAs
         self.aAsI=ASI.aAs 
         self.PAsI=ASI.PAs
         self.WrebarI=ASI.Wrebar
-        AST=Reinforcement(self._DT)
+        AST=Reinforcement(self._DT, self._NT)
         self.dAsT= AST.dAs
         self.aAsT=AST.aAs 
         self.PAsT=AST.PAs
@@ -51,15 +56,16 @@ class Beam:
 
 
 class Reinforcement:
-    def __init__(self,position, diameter):
+    def __init__(self,position, diameter, N):
         #First, we calculate the properties for the diferents diameters of the beam's reinforcement
         self.position=position
         self.diameter=diameter
+        self.N=N
     
     def _properties(self):
         ASproperties=_rebar_properties(self.diameter)
         dAs=ASproperties['diameter']
-        aAs=ASproperties['area']
+        aAs=ASproperties['area']*self.N
         PAs=ASproperties['perimeter']
         Wrebar=ASproperties['weight']
 
