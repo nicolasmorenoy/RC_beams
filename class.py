35da99043b7584f3ca640cd7ad35bcdf0a80ad36
Nicolás <nicolasmorenoy@gmail.com>
2021-08-29 01:51:42 -0500

    Archivo de clases funcional

--- class.py
+++ class.py
@@ -1,6 +1,7 @@
+from VigasCR import properties
 import math
 PI=math.pi
-from functions import _rebar_properties, Mn
+from functions import rebar_properties, Mn
 
 
 class Beam:
@@ -10,69 +11,59 @@ class Beam:
     # ST Longitudinal separation of transverse reinforcement bars
     # fc and fy the material properties
     def __init__(self, name, b, h, r,NS,DS,NI, DI, NT, DT, ST, fc, fy):
-        self._name= name
-        self._b=b
-        self._h=h
-        self._r=r
-        self._NS=NS
-        self._DS=DS
-        self._NI=NI
-        self._DI=DI
-        self._NT=NT
-        self._DT=DT
-        self._ST=ST
+        self.name= name
+        self.b=b
+        self.h=h
+        self.r=r
+        self.NS=NS
+        self.DS=DS
+        self.NI=NI
+        self.DI=DI
+        self.NT=NT
+        self.DT=DT
+        self.ST=ST
         self.fc=fc
         self.fy=fy
-
-    #With this method the script determine de rebar properties for this particular beam
-    
-    def reinforcement_bars_properties(self):
-        ASS=Reinforcement(self._DS, self._NS)
+        ASS=Reinforcement(self.DS, self.NS)
         self.dAsS= ASS.dAs
         self.aAsS=ASS.aAs 
         self.PAsS=ASS.PAs
         self.WrebarS=ASS.Wrebar
-        ASI=Reinforcement(self._DI, self._NI)
+        ASI=Reinforcement(self.DI, self.NI)
         self.dAsI= ASI.dAs
         self.aAsI=ASI.aAs 
         self.PAsI=ASI.PAs
         self.WrebarI=ASI.Wrebar
-        AST=Reinforcement(self._DT, self._NT)
+        AST=Reinforcement(self.DT, self.NT)
         self.dAsT= AST.dAs
         self.aAsT=AST.aAs 
         self.PAsT=AST.PAs
         self.WrebarT=AST.Wrebar
-           
+        self.Ag=self.b*self.h
+        self.ds=self.h-self.r-self.dAsT-self.dAsS/2
+        self.di=self.h-self.r-self.dAsT-self.dAsI/2
+        self.Ig=self.b*self.h**3/12
+        self.ro_S=self.aAsS/self.b/self.ds
+        self.ro_I=self.aAsI/self.b/self.di   
+        self.Mn_S=Mn(self.b,self.ds,self.fc,self.fy,self.ro_S)
+        self.Mn_I=Mn(self.b,self.di,self.fc,self.fy,self.ro_I)
     
-    def _beam_properties(self):
-        self.Ag=self._b*self._h
-        self.ds=self._h-self._r-self.dAsT-self.dAsS/2
-        self.di=self._h-self._r-self.dAsT-self.dAsI/2
-        self.Ig=self._b*self._h**3/12
-        self.ro_S=self._As_S/self._b/self.ds
-        self.ro_I=self._As_I/self._b/self.di
 
-    
-    def _nominal_properties(self):
-        self.Mn_S=Mn(self._b,self.ds,self.fc,self.fy,self.ro_S)
-        self.Mn_I=Mn(self._b,self.di,self.fc,self.fy,self.ro_I)
+    def print_properties(self):
+        print()
 
 
 class Reinforcement:
-    def __init__(self,position, diameter, N):
+    def __init__(self, diameter, N):
         #First, we calculate the properties for the diferents diameters of the beam's reinforcement
-        self.position=position
         self.diameter=diameter
         self.N=N
-    
-    def _properties(self):
-        ASproperties=_rebar_properties(self.diameter)
-        dAs=ASproperties['diameter']
-        aAs=ASproperties['area']*self.N
-        PAs=ASproperties['perimeter']
-        Wrebar=ASproperties['weight']
+        ASproperties=rebar_properties(self.diameter)
+        self.dAs=ASproperties['diameter']
+        self.aAs=ASproperties['area']*self.N
+        self.PAs=ASproperties['perimeter']
+        self.Wrebar=ASproperties['weight']
 
-        return dAs, aAs, PAs, Wrebar
-
-
+Viga1=Beam('Viga1', 0.3,0.3,.04,2,5,2,5,2,3,.1,28,420)
+print(Viga1.Mn_S, Viga1.ro_S)
 
