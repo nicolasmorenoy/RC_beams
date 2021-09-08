20723a6a911dcacf226164a2da39817ee4c82401
unknown <nicolasmorenoy@gmail.com>
2021-09-08 00:45:43 -0500

    last main version

--- main.py
+++ main.py
@@ -2,31 +2,6 @@ from classes import ConcreteProperties, Rectangular, Concrete, Reinforcement, Lo
 from functions import header, print_welcome, _read_properties, _beam_reinforcement
 
 
-
-
-
-# region Test area
-# geometry = Rectangular(0.3, 0.3)
-# material = Concrete(420, 28, 0.04)
-# reinforcement = Reinforcement(LongitudinalReinforcement(5, 2),
-#                               LongitudinalReinforcement(5, 2),
-#                               TransverseReinforcement(3, 0.3, 2))
-# beam = BeamSection('TestBeam', ConcreteProperties(geometry,material,reinforcement))
-# print(geometry.__dict__)
-# beam.getTopNominalMoment()
-# beam.getNominalShearStrength()
-
-# beam.properties.geometry.height = 0.5
-
-# beam.getTopNominalMoment()
-# beam.getNominalShearStrength()
-# BeamSection.Number_of_beams()
-# BeamSection.List_of_beams()
-# print(beam)
-
-
-#end region
-
 def main():
     header(print_welcome)
     beam_name=input('Enter the beam name: ')
@@ -38,18 +13,8 @@ def main():
                                   LongitudinalReinforcement(bottom_diameter, amount_bottom_rebar),
                                   TransverseReinforcement(stirrups_diameter, stirrups_spacing, stirrups_legs))
     beam = BeamSection(beam_name, ConcreteProperties(geometry, material, reinforcement))
-    print(geometry.__dict__)
     beam.getTopNominalMoment()
     beam.getNominalShearStrength()
-
-    beam.properties.geometry.height = 0.5
-
-    beam.getTopNominalMoment()
-    beam.getNominalShearStrength()
-    BeamSection.Number_of_beams()
-    BeamSection.List_of_beams()
-    print(beam)
-
 
 
 if __name__=='__main__':
